f9eab140dd3526cab1d58f739cc8986fab5f6bd7
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-07-13 22:51:38 -0400

    Created handleCustomTip function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div className="container">
@@ -29,44 +31,99 @@ function CalculatorApp() {
 }
 
 function Inputs() {
+  const [bill, setBill] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState("");
+
+  function handleBillChange(value) {
+    setBill(value);
+  }
+
+  function handlePeopleChange(value) {
+    setNumberOfPeople(value);
+  }
+
   return (
     <div className="inputs">
-      <Input label="Bill" src="images/icon-dollar.svg" alt="Dollar icon" />
+      <Input
+        label="Bill"
+        src="images/icon-dollar.svg"
+        alt="Dollar icon"
+        value={bill}
+        onChange={handleBillChange}
+      />
       <TipSelection />
       <Input
         label="Number of People"
         src="images/icon-person.svg"
         alt="Person icon"
+        value={numberOfPeople}
+        onChange={handlePeopleChange}
       />
     </div>
   );
 }
 
-function Input({ label, src, alt }) {
+function Input({ label, src, alt, value, onChange }) {
   return (
     <div className="input">
       <label className="input__label">{label}</label>
       <div className="input__container">
         <img className="input__icon" src={src} alt={alt} />
-        <input className="input__element" type="text" placeholder="0" />
+        <input
+          className="input__element"
+          type="text"
+          placeholder="0"
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
       </div>
     </div>
   );
 }
 
 function TipSelection() {
+  const [tipSelected, setTipSelected] = useState(null);
+  const [customTip, setCustomTip] = useState("");
+  const customTipPercentage = convertPercentage(customTip);
+
+  function handleTipSelected(value) {
+    setTipSelected(value);
+    setCustomTip("");
+  }
+
+  function handleCustomTip(value) {
+    if (isNaN(Number(value))) return;
+    setCustomTip(value === "" ? "" : Number(value));
+    setTipSelected(null);
+  }
+
   const percentages = ["5%", "10%", "15%", "25%", "50%"];
+
+  function convertPercentage(percentage) {
+    const number = parseInt(percentage, 10);
+    return isNaN(number) ? 0 : number / 100;
+  }
 
   return (
     <div className="tips">
       <span className="tips__select-tip">Select Tip %</span>
       <div className="tips__container">
         {percentages.map((percentage) => (
-          <div key={percentage} className="tips__tip">
+          <div
+            key={percentage}
+            className="tips__tip"
+            onClick={() => handleTipSelected(convertPercentage(percentage))}
+          >
             {percentage}
           </div>
         ))}
-        <input className="tips__custom-tip" type="text" placeholder="Custom" />
+        <input
+          className="tips__custom-tip"
+          type="text"
+          placeholder="Custom"
+          value={customTip}
+          onChange={(e) => handleCustomTip(e.target.value)}
+        />
       </div>
     </div>
   );
