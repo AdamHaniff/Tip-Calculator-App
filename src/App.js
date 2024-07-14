e7c536e7ba08932c415575d5badc14b6d6a0b6d0
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-07-14 18:14:38 -0400

    Organized CalculatorApp component better using component composition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+const percentages = ["5%", "10%", "15%", "25%", "50%"];
+
+function convertPercentage(percentage) {
+  const number = parseInt(percentage, 10);
+  return isNaN(number) ? 0 : number / 100;
+}
+
 export default function App() {
   return (
     <div className="container">
@@ -22,45 +29,98 @@ function Header() {
 }
 
 function CalculatorApp() {
+  // STATE
+  const [bill, setBill] = useState("");
+  const [numberOfPeople, setNumberOfPeople] = useState("");
+  const [tipSelected, setTipSelected] = useState(null);
+  const [customTip, setCustomTip] = useState("");
+
+  // VARIABLES
+  const customTipPercentage = convertPercentage(customTip);
+
+  const tipAmount = calculateTipAmount(
+    tipSelected,
+    bill,
+    numberOfPeople,
+    customTipPercentage
+  );
+
+  const totalAmount = tipSelected
+    ? bill + (bill * tipSelected) / numberOfPeople
+    : bill + (bill * customTipPercentage) / numberOfPeople;
+
+  function calculateTipAmount(
+    tipSelected,
+    bill,
+    numberOfPeople,
+    customTipPercentage
+  ) {
+    // const tipAmount = tipSelected
+    //   ? (bill * tipSelected) / numberOfPeople
+    //   : (bill * customTipPercentage) / numberOfPeople;
+    // return Number.isNaN(tipAmount) ? "0.00" : tipAmount.toFixed(2);
+    // I DONT WANT TO HARDCODE THE 0.00 - WILL CHECK IF EACH VALUE EXISTS
+  }
+
+  // HANDLER FUNCTIONS
+  function handleChange(value, setter) {
+    if (isNaN(Number(value))) return;
+    setter(value === "" ? "" : Number(value));
+  }
+
+  function handleBillChange(value) {
+    handleChange(value, setBill);
+  }
+
+  function handlePeopleChange(value) {
+    handleChange(value, setNumberOfPeople);
+  }
+
+  function handleTipSelected(value) {
+    setTipSelected(value);
+    setCustomTip("");
+  }
+
+  function handleCustomTip(value) {
+    if (isNaN(Number(value))) return;
+    setCustomTip(value === "" ? "" : Number(value));
+    setTipSelected(null);
+  }
+
   return (
     <div className="app">
-      <Inputs />
-      <Calculate />
+      <Inputs>
+        <Input
+          label="Bill"
+          src="images/icon-dollar.svg"
+          alt="Dollar icon"
+          value={bill}
+          onChange={handleBillChange}
+        />
+        <TipSelection
+          handleTipSelected={handleTipSelected}
+          handleCustomTip={handleCustomTip}
+          customTip={customTip}
+        />
+        <Input
+          label="Number of People"
+          src="images/icon-person.svg"
+          alt="Person icon"
+          value={numberOfPeople}
+          onChange={handlePeopleChange}
+        />
+      </Inputs>
+
+      <Calculate>
+        <CalculateLabel label="Tip Amount" value={tipAmount} />
+        <CalculateLabel label="Total" value={totalAmount} />
+      </Calculate>
     </div>
   );
 }
 
-function Inputs() {
-  const [bill, setBill] = useState("");
-  const [numberOfPeople, setNumberOfPeople] = useState("");
-
-  function handleBillChange(value) {
-    setBill(value);
-  }
-
-  function handlePeopleChange(value) {
-    setNumberOfPeople(value);
-  }
-
-  return (
-    <div className="inputs">
-      <Input
-        label="Bill"
-        src="images/icon-dollar.svg"
-        alt="Dollar icon"
-        value={bill}
-        onChange={handleBillChange}
-      />
-      <TipSelection />
-      <Input
-        label="Number of People"
-        src="images/icon-person.svg"
-        alt="Person icon"
-        value={numberOfPeople}
-        onChange={handlePeopleChange}
-      />
-    </div>
-  );
+function Inputs({ children }) {
+  return <div className="inputs">{children}</div>;
 }
 
 function Input({ label, src, alt, value, onChange }) {
@@ -81,29 +141,7 @@ function Input({ label, src, alt, value, onChange }) {
   );
 }
 
-function TipSelection() {
-  const [tipSelected, setTipSelected] = useState(null);
-  const [customTip, setCustomTip] = useState("");
-  const customTipPercentage = convertPercentage(customTip);
-
-  function handleTipSelected(value) {
-    setTipSelected(value);
-    setCustomTip("");
-  }
-
-  function handleCustomTip(value) {
-    if (isNaN(Number(value))) return;
-    setCustomTip(value === "" ? "" : Number(value));
-    setTipSelected(null);
-  }
-
-  const percentages = ["5%", "10%", "15%", "25%", "50%"];
-
-  function convertPercentage(percentage) {
-    const number = parseInt(percentage, 10);
-    return isNaN(number) ? 0 : number / 100;
-  }
-
+function TipSelection({ handleTipSelected, handleCustomTip, customTip }) {
   return (
     <div className="tips">
       <span className="tips__select-tip">Select Tip %</span>
@@ -129,14 +167,11 @@ function TipSelection() {
   );
 }
 
-function Calculate() {
+function Calculate({ children }) {
   return (
     <div className="calculate">
       <div className="calculate__container">
-        <div className="calculate__labels">
-          <CalculateLabel label="Tip Amount" />
-          <CalculateLabel label="Total" />
-        </div>
+        <div className="calculate__labels">{children}</div>
         <button className="calculate__reset-btn" type="button">
           Reset
         </button>
@@ -145,14 +180,14 @@ function Calculate() {
   );
 }
 
-function CalculateLabel({ label }) {
+function CalculateLabel({ label, value }) {
   return (
     <div className="calculate__labels-value">
       <div className="calculate__label-sublabel">
         <span className="calculate__label">{label}</span>
         <span className="calculate__sublabel">/ person</span>
       </div>
-      <span className="calculate__value">$4.27</span>
+      <span className="calculate__value">${value}</span>
     </div>
   );
 }
