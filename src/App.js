22d1caed95e185d4af363d950f84894817e2c7e6
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-07-29 23:59:10 -0400

    Gave calculate__value a conditional class

--- src/App.js
+++ src/App.js
@@ -329,13 +329,25 @@ function Calculate({ children, resetBtn }) {
 }
 
 function CalculateLabel({ label, value }) {
+  const isLessThanThousand = Number(value) < 1000;
+
   return (
     <div className="calculate__labels-value">
-      <div className="calculate__label-sublabel">
+      <div
+        className={`calculate__label-sublabel ${
+          isLessThanThousand ? "responsive-margin-right" : ""
+        }`}
+      >
         <span className="calculate__label">{label}</span>
         <span className="calculate__sublabel">/ person</span>
       </div>
-      <span className="calculate__value">${value}</span>
+      <span
+        className={`calculate__value ${
+          isLessThanThousand ? "larger-font" : ""
+        }`}
+      >
+        ${value}
+      </span>
     </div>
   );
 }
